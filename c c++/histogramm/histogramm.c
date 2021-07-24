420a8be37eafe91d9e3c860ce78dbf32081406ed
Thibaut Temkeng <thibaut.temkeng@student.uni.augsburg.de>
2021-07-24 13:48:41 +0200

    [HISTOGRAMM] Add documentation

--- c c++/histogramm/histogramm.c
+++ c c++/histogramm/histogramm.c
@@ -1,4 +1,3 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,85 +6,50 @@
 #include "histogramm.h"
 
 
-Histogram *getHistogram(FILE *file);
-
-int *split(char *line, ssize_t length);
-
-int *max(const int *list, size_t length);
-
-void printList(const int *list, ssize_t length){
+__attribute__((unused)) void printList(const int *list, size_t length) {
     printf("\n");
-    for (int i = 0; i < length; i++){
+    for (int i = 0; i < length; i++) {
         printf("%i ", list[i]);
     }
 
 }
+
 int main() {
     clock_t t = clock();
-    FILE *fp;
     Histogram *h;
     char *filename = "data/image.txt";
-    int map_y[MAX] = {[0 ... MAX-1] = 0};
-
-    fp = fopen(filename, "r");
-    if (fp == NULL) {
-        printf("Daten kann nicht geoeffnet werden\n");
-        exit(EXIT_FAILURE);
-    }
-    h= getHistogram(fp);
+    h = getHistogram(filename);
     normalize(h);
-    
-    printf("\n");
-    int counter = ROW + 1;
-
-    for (int i = 0; i <= ROW ; i++){
-        for (int j = 0; j <= COL ; j++){
-            if(i == ROW  && j == 0){
-                printf("0" );
-            } else if (i==0 && j ==0){
-                printf("%d ", h->max_h);
-            }else if (i==ROW && j == COL+1){
-                printf("  %d ", COL);
-            }
-            else if ( j == 0 || j == COL){
-                printf("|");
-            }else if ( i == ROW){
-                printf("-");
-            }else if (h->data[j-1] == counter-1){
-                printf("*");
-                h->data[j-1]--;
-            }else{
-                printf(" ");
-            }
-        }
-        counter--;
-        printf("\n");
-    }
-    free(fp);
+    plot(h);
     free(h);
     t = clock() - t;
-    double time_taken = ((double)t)/CLOCKS_PER_SEC; // Die verstrichene Zeit berechnen
-    printf("EXECUTION TIME %.3f seconds\n", time_taken);
+    double time_taken = ((double) t) / CLOCKS_PER_SEC; // Die verstrichene Zeit berechnen
+    printf("Ausfuehrungszeit %.3f Sekunde\n", time_taken);
+
     return 0;
 }
 
 
-
-Histogram *getHistogram(FILE *file) {
-    int m = 0;
-    int n = 0;
-    size_t len = 0;
-    char *line = NULL;
+Histogram *getHistogram(char *filename) {
+    float m = 0;
+    float n = 0;
+    size_t len = 1024;
+    char line[len];
     char *tmp = NULL;
     int *hist = calloc(MAX, sizeof(int));
-    // Histogram *result = INIT_HISTOGRAM;
     Histogram *result = malloc(sizeof(Histogram));
-    *result = INIT_HISTOGRAM;
-
+    *result = INIT_HISTOGRAM; // Histogramm initialisieren
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Daten kann nicht geöffnet werden\n");
+        exit(EXIT_FAILURE);
+    }
     // Zeilenweise lesen
-    while (getline(&line, &len, file) != -1) {
+//    while (getline(&line, &len, file) != -1) {
+    while (fgets(line, 1024, file) != NULL) {
         // Zeile aufteilen
         m++;
+//        printf("%s", line);
         n = 1;
         hist[atoi(strtok(line, SEP))]++;
         while ((tmp = strtok(NULL, SEP))) {
@@ -93,13 +57,13 @@ Histogram *getHistogram(FILE *file) {
             n++;
         }
     }
-    // Stastistic
+    // Statistic
     for (int i = 0; i < MAX; ++i) {
         result->sum += hist[i]; //SUM
-        result->avg += i* hist[i]; // AVG
-        result->variance += i*i* hist[i]; // Variance
+        result->avg += i * hist[i]; // AVG
+        result->variance += i * i * hist[i]; // Variance
         // MIN
-        if(result->min == -1 && hist[i] != 0) {
+        if (result->min == -1 && hist[i] != 0) {
             result->min = i;
         }
         // MAX
@@ -107,74 +71,93 @@ Histogram *getHistogram(FILE *file) {
             result->max = i;
             result->dynamic += 1;
         }
-        if (hist[i] > result->max_h){
+        if (hist[i] > result->max_h) {
             result->max_h = hist[i];
-            }
-        if (hist[i] < result->min_h){
+        }
+        if (hist[i] < result->min_h) {
             result->min_h = hist[i];
-            }
-        
-        // Cumulative summe
-        result->cumsum[i] =result->sum;    
+        }
+
+        // Kumulative summe
+        result->cumsum[i] = result->sum;
+        result->cols = (int) n; // Anzahl von Spalten
+        result->rows = (int) m; // Anzahl von Zeilen
         // variance and standard variance 
-        if(i + 1 == MAX){
-            result->variance = (result->variance - result->avg*result->avg/(m*n))/(m*n);
-            result->std = sqrt(result->variance);
-            result->avg /= m*n;
+        if (i + 1 == MAX) {
+            result->variance = (result->variance - result->avg * result->avg / (m * n)) / (m * n);
+            result->std = sqrtf(result->variance);
+            result->avg /= m * n;
         }
     }
     result->data = hist;
     print(result);
+    fclose(file);
     return result;
 }
-void normalize(Histogram *h ){
-    int index =0;
-    int new_min = INT_MAX;
-    int new_max = INT_MIN;
-    int tmp[COL] = {[0 ... COL-1] = 0};
 
-    // Qunatisierung
-    for (int i = 0; i < MAX; i++){
-        index = (int) ((1.0*COL*i)/(float) MAX);
-        tmp[index] += h->data[i];
-        if (tmp[index] < new_min) new_min = tmp[index];
-        if (tmp[index] > new_max) new_max = tmp[index];
+
+void normalize(Histogram *h) {
+    int new_index;
+    int tmp[COL] = {[0 ... COL - 1] = 0};
+
+    h->max_h = INT_MIN;
+    h->min_h = INT_MAX;
+    // Quantisierung
+    for (int i = 0; i < MAX; i++) {
+        new_index = (int) ((1.0 * COL * i) / (float) MAX);
+        tmp[new_index] += h->data[i];
+        if (tmp[new_index] < h->min_h) h->min_h = tmp[new_index];
+        if (tmp[new_index] > h->max_h) h->max_h = tmp[new_index];
     }
-    h->max_h = new_max;
-    h->min_h = new_min;
-    float abstand = new_max - new_min;
-    
-    // neue Werten zuweisen
-    int temp[MAX] = {[0 ... MAX-1]=0};
+    float abstand = (float) (h->max_h - h->min_h);
+
+    // Neue Werten zuweisen
+    int temp[MAX] = {0};
     h->data = temp;
-    for (int i=0; i < COL; i++){ 
+    for (int i = 0; i < COL; i++) {
         if (tmp[i] != 0) {
-        h->data[i] = (int) (float) ROW*((tmp[i]- new_min)/abstand);
-        h->data[i] = h->data[i] == 0 ? 1 : h->data[i];
+            h->data[i] = (int) ((float) (ROW * (tmp[i] - h->min_h)) / abstand);
+            h->data[i] = h->data[i] == 0 ? 1 : h->data[i];
         }
     }
-    
+
 }
 
-int *split(char *line, ssize_t length) {
-    int result[length];
-    int i = 1;
-    char *tmp;
+void print(Histogram *h) {
+    printf("############ Histogramm #########\n");
+    printf("\n# %9s %d", "MIN:", h->min);
+    printf("\n# %9s %d", "MAX:", h->max);
+    printf("\n# %9s %d", "SUM:", h->sum);
+    printf("\n# %9s %.3f", "AVG:", h->avg);
+    printf("\n# %9s %.3f", "STD:", h->std);
+    printf("\n# %9s %d", "MAX_H:", h->max_h);
+    printf("\n# %9s %d", "MIN_H:", h->min_h);
+    printf("\n# %9s %d", "DYNAMIC:", h->dynamic);
+    printf("\n# VARIANCE: %.3f\n", h->variance);
+    printf("##################################\n");
+//    printf("# Anzahl von Zeilen: %d\n", h->rows);
+//    printf("# Anzahl von Spalten: %d\n", h->cols);
+//    printf("##################################\n");
 
-    result[0] = atoi(strtok(line, SEP));
-    while ((tmp = strtok(NULL, SEP))) {
-        result[i++] = atoi(tmp);
+}
+
+void plot(Histogram *h) {
+    int counter = ROW + 1;
+    printf("\n");
+    for (int i = 0; i <= ROW; i++) {
+        for (int j = 0; j <= COL; j++) {
+            if (i == ROW && j == 0) printf("0");
+            else if (i == 0 && j == 0) printf("%d ", h->max_h);
+            else if (i == ROW && j == COL + 1) printf("  %d ", COL);
+            else if (j == 0 or j == COL) printf("|");
+            else if (i == ROW) printf("-");
+            else if (h->data[j - 1] == counter - 1 and i != 0) {
+                printf("*");
+                h->data[j - 1]--;
+            } else printf(" ");
+        }
+        counter--;
+        printf("\n");
     }
-}
 
-void print(Histogram *h){
-    printf("\n     MIN: %d", h->min);
-    printf("\n     MAX: %d", h->max);
-    printf("\n     SUM: %d", h->sum);
-    printf("\n     AVG: %.3f", h->avg);
-    printf("\n     STD: %.3f", h->std);
-    printf("\n   MAX_H: %d", h->max_h);
-    printf("\n   MIN_H: %d", h->min_h);
-    printf("\n DYNAMIC: %d", h->dynamic);
-    printf("\nVARIANCE: %.3f\n", h->variance);
 }
--- c c++/histogramm/histogramm.h
+++ c c++/histogramm/histogramm.h
@@ -5,29 +5,48 @@
 
 #define MAX 256
 #define SEP " "
-#define ROW 95
-#define COL 150
-
-typedef struct  Histogram_ {
+#define ROW 50
+#define COL 200
+#define and &&
+#define or ||
+typedef struct Histogram_ {
     int sum;
-    int max ;
-    int min ;
+    int max;
+    int min;
     float avg;
     float std;
     float variance;
     int max_h;
     int min_h;
     int dynamic;
-    int* cumsum;
-    int* data;
+    int *cumsum;
+    int *data;
+    int cols, rows;
+
 } Histogram;
 
-// int ptr[MAX] = {[0 ... MAX-1] = 0};
-// int data[MAX] = {[0 ... MAX-1] = 0};
 int ptr[MAX] = {0};
 int data[MAX] = {0};
-Histogram INIT_HISTOGRAM  = {0,0,-1,0,0,0, INT_MIN, INT_MAX, 0, ptr, data};
+// int ptr[MAX] = {[0 ... MAX-1] = 0};
+// int data[MAX] = {[0 ... MAX-1] = 0};
+Histogram INIT_HISTOGRAM = {.sum=0, .max=0, .min=-1, .avg=0, .std=0, .variance=0, .max_h=INT_MIN, .min_h= INT_MAX,
+        .dynamic=0, .cumsum= ptr, .data=data, .cols=0, .rows=0};
 
+/*
+ * Histogramm plotten
+ */
+void plot(Histogram *h);
+/*
+ * Gibt fast alle Eigenschaft des Histogramms `h`
+ */
 void print(Histogram *h);
+/*
+ * Normalisiere das Histogramm
+ * Die Werte des Histogramm werden in `[0, COL]´ und `[0, ROW]`
+ * @param h Der Histogramm
+ */
 void normalize(Histogram *h);
+
+Histogram *getHistogram(char *filename);
+
 #endif
